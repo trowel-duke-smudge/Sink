af87069cacc4cd2158e14c2244da2fadda279df3
trowel-duke-smudge <trowel-duke-smudge@duck.com>
2026-06-07 02:01:08 -0700

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import NumberFlow from '@number-flow/vue'
 import { Menu, Star, X } from 'lucide-vue-next'
-<!-- import { GitHubIcon, TelegramIcon, XIcon } from 'vue3-simple-icons' -->
-
 const showMenu = ref(false)
-<!-- const { title, telegram, twitter, github } = useAppConfig()
 const { rawStats } = useGithubStats() -->
 </script>
 
